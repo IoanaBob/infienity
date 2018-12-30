0d00220c5b3fb066fea85cba7f22c5a52d557756
Ioana Surdu-Bob <ioana.sbob@gmail.com>
2018-12-30 15:54:40 +0100

    made state variables optional, and generalized model var name based on assign

--- lib/infienity/commander.rb
+++ lib/infienity/commander.rb
@@ -5,56 +5,78 @@ module Infienity
         current_class = field.to_s.singularize.classify.constantize
 
         define_method "paginate_#{field}" do |pagination_params:, is_first_load: false|
-          search_opts = { attribute: state.search_attribute, search_string: state.search_string }
-          sort_opts = state.sorting_dropdown_options.fetch(state.selected_dropdown_option)
+          sorting_dropdown_options = state.try(:sorting_dropdown_options)
+          selected_dropdown_option = state.try(:selected_dropdown_option)
+          search_attribute = state.try(:search_attribute)
+          search_string = state.try(:search_string)
+          per_page = state.try(:per_page) || 10
 
-          return if (state.per_page * state.index) > current_class.paginate_count(search: search_opts)
+          search_opts = { attribute: search_attribute, search_string: search_string } if search_string.present?
+          sort_opts = sorting_dropdown_options&.fetch(selected_dropdown_option)
+
+          return if (per_page * state.index) > current_class.paginate_count(search: search_opts)
 
           state.index = state.index + 1
-          state.users = state.users + current_class.paginate(per_page: state.per_page, start: state.index, search: search_opts, sort: sort_opts)
+          state.send("#{field.to_sym}=", state.send(field.to_sym) + current_class.paginate(per_page: per_page, start: state.index, search: search_opts, sort: sort_opts))
 
           execute_js_function('Infienity.fieResponded', is_first_load)
         end
 
         define_method "filter_#{field}" do
-          search_opts = { attribute: state.search_attribute, search_string: state.search_string }
-          sort_opts = state.sorting_dropdown_options.fetch(state.selected_dropdown_option)
+          sorting_dropdown_options = state.try(:sorting_dropdown_options)
+          selected_dropdown_option = state.try(:selected_dropdown_option)
+          search_attribute = state.try(:search_attribute)
+          search_string = state.try(:search_string)
+          per_page = state.try(:per_page) || 10
+
+          search_opts = { attribute: search_attribute, search_string: search_string } if search_string.present?
+          sort_opts = sorting_dropdown_options&.fetch(selected_dropdown_option)
           state.index = 0
 
-          state.users = current_class.paginate(per_page: state.per_page, start: 0, search: search_opts, sort: sort_opts)
+          state.send("#{field.to_s}=", current_class.paginate(per_page: per_page, start: 0, search: search_opts, sort: sort_opts))
 
           execute_js_function('Infienity.fieResponded', true)
         end
 
         define_method "sort_#{field}" do |sort:|
-          unless state.sorting_dropdown_options.key?(state.selected_dropdown_option)
+          selected_dropdown_option = state.try(:selected_dropdown_option)
+          search_attribute = state.try(:search_attribute)
+          search_string = state.try(:search_string)
+          per_page = state.try(:per_page) || 10
+
+          unless state.sorting_dropdown_options.key?(selected_dropdown_option)
             raise StandardError("dropdown option not provided in the correct format")
           end
 
           state.selected_dropdown_option = sort.keys.first
 
-          search_opts = { attribute: state.search_attribute, search_string: state.search_string }
+          search_opts = { attribute: search_attribute, search_string: search_string } if search_string.present?
           sort_opts = sort.values.first
           state.index = 0
 
-          state.users = current_class.paginate(per_page: state.per_page, start: 0, search: search_opts, sort: sort_opts)
+          state.send("#{field.to_s}=", current_class.paginate(per_page: per_page, start: 0, search: search_opts, sort: sort_opts))
 
           execute_js_function('Infienity.fieResponded', true)
         end
 
         define_method "sort_select_#{field}" do
-          unless state.sorting_dropdown_options.key?(state.selected_dropdown_option)
+          selected_dropdown_option = state.try(:selected_dropdown_option)
+          search_attribute = state.try(:search_attribute)
+          search_string = state.try(:search_string)
+          per_page = state.try(:per_page) || 10
+
+          unless state.sorting_dropdown_options.key?(selected_dropdown_option)
             raise StandardError("dropdown option not provided in the correct format")
           end
 
           sort = JSON.parse(@caller[:value])
           state.selected_dropdown_option = sort.keys.first
 
-          search_opts = { attribute: state.search_attribute, search_string: state.search_string }
+          search_opts = { attribute: search_attribute, search_string: search_string } if search_string.present?
           sort_opts = sort.values.first
           state.index = 0
 
-          state.users = current_class.paginate(per_page: state.per_page, start: 0, search: search_opts, sort: sort_opts)
+          state.send("#{field.to_s}=", current_class.paginate(per_page: per_page, start: 0, search: search_opts, sort: sort_opts))
 
           execute_js_function('Infienity.fieResponded', true)
         end
